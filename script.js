f6c25f6a6247462d2ae972ad788125f542a5c848
Laura <laurathomazi@gmail.com>
2021-02-04 22:58:26 -0300

    Continuação de criação dos elementos de entrada e saída das transções

--- script.js
+++ script.js
@@ -38,7 +38,13 @@ const transactions = [
         amount: -20000,
         date: '23/01/2021',
     },
-]
+    {
+        id: 4,
+        description: 'App',
+        amount: 200000,
+        date: '23/01/2021',
+    },
+];
 
 /*
     Eu preciso somar as entradas 
@@ -49,13 +55,35 @@ const transactions = [
 
 const Transaction = {
     incomes() {
-        //somar as entradas
+        let income = 0;
+        //pegar todas as transações 
+        //e para cada transação,
+        transactions.forEach(transaction => {
+            //se for maior que zero 
+            if(transaction.amount > 0) {
+                //somar a uma variavel e retornar a variável 
+                income += transaction.amount;
+                 // igual a income = income + transaction.amount
+            }      
+        })        
+        return income;
     },
     expenses() {
-        //somar as saídas
+        let expense = 0;
+        //pegar todas as transações 
+        //e para cada transação,
+        transactions.forEach(transaction => {
+            //se for menor que zero 
+            if(transaction.amount < 0) {
+                //somar a uma variavel e retornar a variável 
+                expense += transaction.amount;
+                 // igual a expense = expense + transaction.amount
+            }          
+        })        
+        return expense;
     },
     total() {
-        //entradas de saídas
+        return "Discover"
     }
 }
 
@@ -64,9 +92,9 @@ const Transaction = {
 
     Estavam todos descritos manualmente no HTML na tag <tbody>
 */
+//criando elemento responsável por criar transações
 
 const DOM = {
-    //criando elemento responsável por criar transações
     transactionsContainer: document.querySelector('#data-table tbody'),
 
     addTransaction(transaction, index) {
@@ -75,24 +103,56 @@ const DOM = {
         
         DOM.transactionsContainer.appendChild(tr)
     },
+
     innerHTMLTransaction(transaction) {
+        const CSSclass = transaction.amount > 0 ? "income" : "expense"
+
+        const amount = Utils.formatCurrency(transaction.amount)
+
         const html = `
-            <td class="description">Luz</td>
-            <td class="expense">- R$ 500,00</td>
-            <td class="date">23/01/2021</td>
+            <td class="description">${transaction.description}</td>
+            <td class="${CSSclass}">${amount}</td>
+            <td class="date">${transaction.date}</td>
             <td>
                 <img src="./assets/minus.svg" alt="Remover Transação">
             </td>
          `
-
          return html
+    },
+    //atualização dos cards de entradas, saídas e total
+    updateBalance() {
+        document
+            .getElementById('incomeDisplay')
+            .innerHTML = Transaction.incomes()
+        document
+            .getElementById('expenseDisplay')
+            .innerHTML = Transaction.expenses()
+        document
+            .getElementById('totalDisplay')
+            .innerHTML = Transaction.total()
     }
 }
 
-DOM.addTransaction(transactions[0])
-DOM.addTransaction(transactions[1])
-DOM.addTransaction(transactions[2])
+const Utils = {
+    formatCurrency(value) {
+        const signal = Number(value) < 0 ? "-" : ""
+       
+        value = String(value).replace(/\D/g, "")
 
-transaction.forEach(element => {
-    
-});
+        value = Number(value) / 100
+
+        value = value.toLocaleString("pt-BR", {
+            style: "currency", 
+            currency: "BRL"
+        })
+
+        return signal + value
+    }
+}
+
+//para cada transação rode uma funcionalidade. 
+transactions.forEach(function(transaction){
+    DOM.addTransaction(transaction)
+})
+
+DOM.updateBalance()
